bd0824d62ff5e857ab4ca2705f17e3cefbe8104e
ysinh < yysh1020@gmail.com>
2024-04-19 10:47:51 +0900

    data auto create

---
+++ src/test/java/com/example/ms1/Ms1ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.ms1;
+
+import com.example.ms1.note.note.Note;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+class Ms1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+
+    }
+}
