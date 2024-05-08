d8c58067a63f76401bb33982221ccc0ccd990079
ysinh <yysh1020@gmail.com>
2024-05-08 16:58:28 +0900

    노트북 도메인 의존성 관리 ë 리팩토링

--- src/main/java/com/example/ms1/note/note/notebook/NotebookController.java
+++ src/main/java/com/example/ms1/note/note/notebook/NotebookController.java
@@ -11,24 +11,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 @RequiredArgsConstructor
 public class NotebookController {
-    private final NotebookRepository notebookRepository;
+    private final NotebookService notebookService;
     private final NoteService noteService;
+
 
     @PostMapping("/books/write")
     public String write() {
-        Notebook notebook = new Notebook();
-        notebook.setName("새노트북");
 
-        Note note = noteService.saveDefault();
-        notebook.addNote(note);
-
-        notebookRepository.save(notebook);
+        notebookService.saveDefault();
 
         return "redirect:/";
     }
     @GetMapping("/books/{id}")
     public String detail(@PathVariable("id") Long id) {
-        Notebook notebook = notebookRepository.findById(id).orElseThrow();
+        Notebook notebook = notebookService.getNotebook(id);
         Note note = notebook.getNoteList().get(0);
 
         return "redirect:/books/%d/notes/%d".formatted(id, note.getId());
@@ -36,17 +32,19 @@ public class NotebookController {
 
     @PostMapping("/groups/{notebookId}/books/write")
     public String groupWrite(@PathVariable("notebookId") Long notebookId) {
-        Notebook parent = notebookRepository.findById(notebookId).orElseThrow();
+
+        Notebook parent = notebookService.getNotebook(notebookId);
 
         Notebook child = new Notebook();
         child.setName("새노트북");
 
         Note note = noteService.saveDefault();
         child.addNote(note);
-        notebookRepository.save(child);
+
+        notebookService.save(child);
 
         parent.addChild(child);
-        notebookRepository.save(parent);
+        notebookService.save(parent);
 
         return "redirect:/";
     }
--- src/main/java/com/example/ms1/note/note/notebook/NotebookService.java
+++ src/main/java/com/example/ms1/note/note/notebook/NotebookService.java
@@ -1,5 +1,7 @@
 package com.example.ms1.note.note.notebook;
 
+import com.example.ms1.note.note.note.Note;
+import com.example.ms1.note.note.note.NoteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,7 @@ import java.util.List;
 public class NotebookService {
 
     private final NotebookRepository notebookRepository;
+    private final NoteService noteService;
 
     public Notebook getNotebook(Long notebookId) {
         return notebookRepository.findById(notebookId).orElseThrow();
@@ -21,5 +24,14 @@ public class NotebookService {
 
     public List<Notebook> getNotebookList() {
         return notebookRepository.findAll();
+    }
+
+    public void saveDefault() {
+        Notebook notebook = new Notebook();
+        notebook.setName("새노트북");
+
+        notebookRepository.save(notebook);
+        Note note = noteService.saveDefault();
+        notebook.addNote(note);
     }
 }
